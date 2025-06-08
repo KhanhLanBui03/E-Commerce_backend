1feaeac360fa59bc9484199f75bf7b3b8b37a96e
Khang Lê <duykhang12a7@gmail.com>
2025-06-08 21:12:27 +0700

    first init

--- middleware/multer.js
+++
@@ -1,8 +0,0 @@
-import multer from "multer";
-
-// Sử dụng memoryStorage để giữ file trong bộ nhớ (file.buffer)
-const storage = multer.memoryStorage();
-
-const upload = multer({ storage });
-
-export default upload;
